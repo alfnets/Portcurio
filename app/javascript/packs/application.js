871e3d398b368884d011d75982aa68a96c44503a
alfnet-info <yunbo25@gmail.com>
2022-07-07 14:09:31 +0900

    modify js setting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,7 @@ import "bootstrap"
 require("additions/jscroll")
 require("additions/micropost_show_open")
 require("additions/user_show_close_by_tab")
-require("render_root")
+require("packs/render/root")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
