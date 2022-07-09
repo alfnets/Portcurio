9371e81bcb2beaa4e3cbea3015830ba3442aab89
alfnet-info <yunbo25@gmail.com>
2022-07-09 13:09:06 +0900

    apply stg, prod

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,9 +22,12 @@
 # wheneverにrailsを起動する必要があるためRails.rootを使用
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
 ENV.each { |k, v| env(k, v) } # 環境変数をwheneverにいい感じに書き込んでもらう
 set :output, error: 'log/crontab_error.log', standard: 'log/crontab.log' # ログを書き出すようにしておくとデバッグが楽
-set :environment, :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
 
 # 早朝4時にキャッシュをクリア
 every 1.day, at: '7:00 pm' do
