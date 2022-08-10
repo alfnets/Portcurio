0446019ed531185539c9e9444d8842790d04ca82
alfnet-info <yunbo25@gmail.com>
2022-08-11 08:48:19 +0900

    refactor icon_preview.js

--- app/javascript/packs/icon_preview.js
+++ app/javascript/packs/icon_preview.js
@@ -1,34 +1,24 @@
 $(document).on("turbolinks:load", function () {
   $("#user_image").on("change", function (e) {
     const files = e.target.files;
+    const file = e.target.files[0];
     if (files.length === 0) {
       $("#image_preview").html("");
       $("#icon_image").css({'display':'block'});
     } else {
-      let d = new $.Deferred().resolve();
-      $.each(files, function (i, file) {
-        d = d.then(function () {
-          return previewImage(file);
-        });
-      });
+      console.log("hoge");
+      const reader = new FileReader();
+      const img = new Image();
+      reader.onload = function (e) {
+        // 画像を表示
+        $("#icon_image").css({'display':'none'});
+        $("#image_preview").empty();
+        $("#image_preview").append(img);
+        img.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
     }
   });
-
-  function previewImage(imageFile) {
-    const reader = new FileReader();
-    const img = new Image();
-    const def = $.Deferred();
-    reader.onload = function (e) {
-      // 画像を表示
-      $("#icon_image").css({'display':'none'});
-      $("#image_preview").empty();
-      $("#image_preview").append(img);
-      img.src = e.target.result;
-      def.resolve(img);
-    };
-    reader.readAsDataURL(imageFile);
-    return def.promise();
-  }
 });
 
 require("packs/icon_preview")
