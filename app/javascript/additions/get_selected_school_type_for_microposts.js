eceb3d9b89224294a2de8ba2d232fcf8390de83e
alfnet-info <yunbo25@gmail.com>
2022-08-10 23:36:01 +0900

    fix bug

--- app/javascript/additions/get_selected_school_type_for_microposts.js
+++ app/javascript/additions/get_selected_school_type_for_microposts.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', function() {
   $('#micropost_school_type').on('change', function() {
     const val = $('#micropost_school_type').val();
+    $('#micropost_tags').tagsinput('add', `${val}`);
     $.ajax({
       type: 'GET',
       url: "/microposts/get_selected_school_type",
