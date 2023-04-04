7a03440de4ef8f8dd5f038558333433f76acab61
alfnets <alf@alfnets.info>
2023-04-04 23:09:25 +0900

    feat: grid display with materials in portcurio

--- spec/requests/portcurio_spec.rb
+++ spec/requests/portcurio_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Portcurio", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get portcurio_index_path
+      get portcurio_index_path(micropost: { educational_material: true })
       expect(response).to have_http_status(:success)
     end
   end
